8eb89f8fd7a0b0bb22854edd09b821913e6a5908
marieval <marievally@seznam.cz>
2019-04-02 13:23:59 +0200

    Updates

---
+++ precache-manifest.6115fce407efae9ab068ca2279250c61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec9b99f872ae25e576f1331c9527fe85",
+    "url": "/Wiki-Viewer-React/static/media/magnifier.ec9b99f8.jpg"
+  },
+  {
+    "revision": "0ca7963d742b90195ab9",
+    "url": "/Wiki-Viewer-React/static/js/runtime~main.c3591535.js"
+  },
+  {
+    "revision": "cf19913a39ac59155f83",
+    "url": "/Wiki-Viewer-React/static/js/main.4c599aa9.chunk.js"
+  },
+  {
+    "revision": "53b8e15d171fedf93c13",
+    "url": "/Wiki-Viewer-React/static/js/2.f42f69c1.chunk.js"
+  },
+  {
+    "revision": "cf19913a39ac59155f83",
+    "url": "/Wiki-Viewer-React/static/css/main.ae0fa02a.chunk.css"
+  },
+  {
+    "revision": "e0f924b8a164a86386601fa0dc279b3e",
+    "url": "/Wiki-Viewer-React/index.html"
+  }
+];
