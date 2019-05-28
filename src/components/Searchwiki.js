985a2d915060c72d33bbc3b324084c58b1264073
marieval <majiku@seznam.cz>
2019-05-28 14:38:10 +0200

    convert class-component Searchwiki to functional-component

--- src/components/Searchwiki.js
+++ src/components/Searchwiki.js
@@ -1,37 +1,34 @@
 import React, { Component } from "react";
 import "./Searchwiki.css";
 
-class Searchwiki extends Component {
+const Searchwiki = (props) => {
+    return (
+        <div className="Searchwiki">
+            <h1>Wikipedia Viewer</h1>
+            <form onSubmit={props.formSubmit}>
+                <input
+                    type="search"
+                    placeholder="Type text to search"
+                    onChange={props.inputChange}
+                    autoFocus
+                />
+                <button
+                    className="btn btn--search"
+                    type="submit"
+                    onClick={props.formSubmit}
+                >Search</button>
+            </form>
 
-    render() {
-        return (
-            <div className="Searchwiki">
-                <h1>Wikipedia Viewer</h1>
-                <form onSubmit={this.props.formSubmit}>
-                    <input
-                        type="search"
-                        placeholder="Type text to search"
-                        onChange={this.props.inputChange}
-                        autoFocus
-                    />
-                    <button
-                        className="btn btn--search"
-                        type="submit"
-                        onClick={this.props.formSubmit}
-                    >Search</button>
-                </form>
+            <a href="https://en.wikipedia.org/wiki/Special:Random"
+                target="_blank"
+                rel="noopener noreferrer">
+                <button
+                    className="btn btn--random"
+                    type="submit">Read a random article</button>
+            </a>
+        </div >
+    )
 
-                <a href="https://en.wikipedia.org/wiki/Special:Random"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <button
-                        className="btn btn--random"
-                        type="submit">Read a random article</button>
-                </a>
-
-            </div >
-        )
-    }
-}
+} 
 
 export default Searchwiki;
