507d50225948fed53dd837a58b07a920bdc4e457
marieval <majiku@seznam.cz>
2019-05-28 14:43:08 +0200

    add gitignore

---
+++ build/precache-manifest.8539bb1e13b00907ea3271b50de32c8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec9b99f872ae25e576f1331c9527fe85",
+    "url": "/Wiki-Viewer-React/static/media/magnifier.ec9b99f8.jpg"
+  },
+  {
+    "revision": "ccc68c1e8904a1e1af5e03f7dd6aa1f7",
+    "url": "/Wiki-Viewer-React/static/media/logo-github.ccc68c1e.png"
+  },
+  {
+    "revision": "0ca7963d742b90195ab9",
+    "url": "/Wiki-Viewer-React/static/js/runtime~main.c3591535.js"
+  },
+  {
+    "revision": "f85e86cd64f0c67803ef",
+    "url": "/Wiki-Viewer-React/static/js/main.e5dd1637.chunk.js"
+  },
+  {
+    "revision": "d0ba1e135468a333da48",
+    "url": "/Wiki-Viewer-React/static/js/2.40a45616.chunk.js"
+  },
+  {
+    "revision": "f85e86cd64f0c67803ef",
+    "url": "/Wiki-Viewer-React/static/css/main.536752e1.chunk.css"
+  },
+  {
+    "revision": "e93e8e85bfc3b0cf11ef3cb7b19b6873",
+    "url": "/Wiki-Viewer-React/index.html"
+  }
+];
